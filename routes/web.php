35598bcbf62e8441d843dd100f077d2c17180fff
Collins Bett <collinsbett@Collinss-MacBook-Pro.local>
2021-09-26 17:44:57 +0300

    Create a listings model and make migrations

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::resource('listings', 'ListingsController');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
